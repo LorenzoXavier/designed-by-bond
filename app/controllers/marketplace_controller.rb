f25f7ad834a982350f5dea92c780acb191373be4
Lorenzo Cursley <lorenzo.xavier87@gmail.com>
2022-05-29 20:21:29 +0100

    setting up devise and simple form

---
+++ app/controllers/marketplace_controller.rb
@@ -0,0 +1,4 @@
+class MarketplaceController < ApplicationController
+  def index
+  end
+end
---
+++ test/controllers/marketplace_controller_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class MarketplaceControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get marketplace_index_url
+    assert_response :success
+  end
+
+end
