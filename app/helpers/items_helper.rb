d468f5575a4b7900545cff98c8308a11d6ffa363
Lorenzo Cursley <lorenzo.xavier87@gmail.com>
2022-05-30 20:22:27 +0100

    index page looking good

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,2 +1,14 @@
 module ItemsHelper
+  def allowed_to_edit(item)
+    user_signed_in? && current_user.id == item.user_id || current_user.admin?
+  end
+
+  def product_availability(item)
+    if item.availability == "0"
+      "Not in stock"
+    else
+      "In Stock"
+    end
+
+  end
 end
