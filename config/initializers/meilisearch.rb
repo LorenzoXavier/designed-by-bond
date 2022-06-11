1efac83fb199077b7e41a0b04d9d1c97922601e2
Lorenzo Cursley <lorenzo.xavier87@gmail.com>
2022-06-11 16:00:22 +0100

    changes to meilisearch setup

--- config/initializers/meilisearch.rb
+++ config/initializers/meilisearch.rb
@@ -1,6 +1,15 @@
-MeiliSearch::Rails.configuration = {
-  meilisearch_host: 'http://localhost:7700',
-   meilisearch_api_key: 'YourMeilisearchAPIKey',
-  timeout: 2,
-  max_retries: 1,
-}
+if Rails.env.development?
+  MeiliSearch::Rails.configuration = {
+    meilisearch_host: 'http://localhost:7700',
+     meilisearch_api_key: 'YourMeilisearchAPIKey',
+    timeout: 2,
+    max_retries: 1,
+  }
+else
+  MeiliSearch::Rails.configuration = {
+    meilisearch_host: 'https://sesmarketplace.herokuapp.com',
+     meilisearch_api_key: 'YourMeilisearchAPIKey',
+    timeout: 2,
+    max_retries: 1,
+  }
+end
