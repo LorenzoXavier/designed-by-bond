29cd7f6590bab9d41fb89aa8cf986e98d39df11b
Lorenzo Cursley <lorenzo.xavier87@gmail.com>
2022-07-28 17:27:49 +0100

    removed repetition in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -50,22 +50,6 @@ user = User.new(
 puts "User created"
 user.save!
 
-# 12.times do |i|
-#   Item.create!([{
-#     title: Faker::Lorem.sentence(word_count: 2),
-#     price: Random.rand(10...10000),
-#     review_rating: Random.rand(0...5),
-#     review_count: Random.rand(10...100),
-#     brand: Faker::DcComics.title,
-#     product_code: Faker::Lorem.characters(number: 10),
-#     quantity_available: Random.rand(1...1000),
-#     description: Faker::Lorem.paragraphs(number: 2),
-#     image: Rails.root.join("app/assets/images/engine.jpg").open,
-#     user_id: User.first.id
-#   }])
-#   puts "creating item number #{i + 1}"
-# end
-
 12.times do |i|
   Item.create!([{
     title: Faker::Lorem.sentence(word_count: 2),
